4a82cbc6237a05f682190db31067d34bd6f48481
stanley <stanleyagbanobi@gmail.com>
2024-08-25 17:52:57 +0100

    special offers and customer reviews

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,13 @@
 
-const Button = ({ label, iconURL }) => {
+const Button = ({ label, iconURL, backgroundColor, textColor, borderColor }) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 font-montserrant text-lg leading-none bg-coral-red rounded-full text-white border-coral-red ">
+    <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrant text-lg leading-none rounded-full
+      
+      ${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}` 
+      : "bg-coral-red text-white border-coral-red" } 
+      
+      `}>
+
         {label}
 
         {iconURL && <img 
@@ -9,6 +15,7 @@ const Button = ({ label, iconURL }) => {
         alt="arrow right icon" className="ml-2 rounded-full bg-white w-5 h-5"
          />
         }
+
     </button>
   )
 }
--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import ServiceCard from "../components/ServiceCard"
+import { services } from "../constants"
 
 const Services = () => {
   return (
-    <section>
-      
+    <section className='flex flex-col md:flex-row flex-wrap justify-center gap-9 max-container'>
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service} />
+      ))}
     </section>
   )
 }
