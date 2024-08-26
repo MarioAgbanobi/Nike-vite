86d433051d9a98fd168079303cd748e55b01c144
stanley <stanleyagbanobi@gmail.com>
2024-08-26 02:40:59 +0100

    subscribe and footer

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,12 @@
 
-const Button = ({ label, iconURL, backgroundColor, textColor, borderColor }) => {
+const Button = ({ label, iconURL, backgroundColor, textColor, borderColor, fullWidth }) => {
   return (
     <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrant text-lg leading-none rounded-full
       
       ${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}` 
       : "bg-coral-red text-white border-coral-red" } 
+
+      ${fullWidth && 'w-full'} "}
       
       `}>
 
